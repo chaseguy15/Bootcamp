68aa7739eb32ba4428895605e1264e306385d53e
chaseguy15 <chase.holder13@gmail.com>
2020-09-17 00:56:59 -0500

    test

---
+++ python_basics.py
@@ -0,0 +1,28 @@
+"""
+Python is a must-know language, used for a wide array of applications because
+of how dynamic it is. The language is also user friendly so we will be starting
+with it.
+"""
+
+### COMMENTS ###
+
+# an single-line comment, can be used following statements
+
+"""
+A multi-line comment.
+These are best used for important documentation.
+"""
+
+### BASIC TYPES ###
+
+CONSTANT_VALUE = 3
+# Always capitalized, often found in a separate file
+
+local_variables = 5
+# Low level object given identity which can be operated on
+
+def my_function(x):
+    y = x + 2
+    return y
+# A function, used often to do operation on many different inputs. In python,
+# a function is denoted by "def" and the output is governed by "return".
