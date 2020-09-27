d5288bde359bf057e760ad02c15a693d9772ba7c
chaseguy15 <31543145+chaseguy15@users.noreply.github.com>
2020-09-27 14:09:19 -0500

    Create challenge3.py
    Added challenge 3, obstacle path generation

---
+++ challenges/challenge3.py
@@ -0,0 +1,20 @@
+# CHALLENGE 3
+
+"""
+Given: 2 obstacles
+
+TODO: Generate 2 outer paths from one obstacle to another as the tuple of the
+points the path start/end at.
+"""
+class point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+class obstacle:
+    def __init__(self, radius: float, center: point):
+        self.radius = radius
+        self.center = center
+
+path1 = #(point1, point2)
+path2 = #(point3, point4)
