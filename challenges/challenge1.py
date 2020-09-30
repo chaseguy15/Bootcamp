0ce9d0e8ab1886e37dff6fe3093960d509d7580b
Rishii <rishit.thakkar@gmail.com>
2020-09-29 23:10:02 -0500

    Finished challenge 1

--- challenges/challenge1.py
+++ challenges/challenge1.py
@@ -7,10 +7,14 @@ Once you have completed the challenge, please push this to your remote branch.
 """
 
 # TODO: Create function
-
+def printTimes (x: int, n: int):
+    for iteration in range(n):
+        print(x)
 
 
 # TODO: Test function
-
+x = int(input ("\nWhat's the x value: "))
+n = int(input ("\nHow many times: "))
+printTimes(x,n)
 #x =
 #n =
