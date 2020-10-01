db2bccf8b465a4181af3b2769bd8ab3c2e9b96d1
Rishii <rishit.thakkar@gmail.com>
2020-09-30 19:12:34 -0500

    Updated formula for finding final velocity. Made the final position as instance variables

--- challenges/challenge2.py
+++ challenges/challenge2.py
@@ -13,33 +13,39 @@ import math
 class dropCalc:
     def __init__(self, x_velocity: float, z_velocity: float, height: float):
         self.xV = x_velocity
-        self.zV = z_velocity
+        self.zVu = z_velocity #intial z_velocity
+        self.zVf = 0 #final velocity
         self.height = height
         self.time = 0
         self.ACC_DUE_TO_GRAV = 9.81 #constant
+        self.x = 0
+        self.y = 0
+        self.z = 0
 
     #I'm assuming the object starts at (0,h)
     def calcTime(self):
         # arbitrary test: time = 2.232
-        time = math.sqrt(2*self.ACC_DUE_TO_GRAV*self.height + (self.zV**2))
-        time = time - self.zV
+        self.zVf = -math.sqrt(2*self.ACC_DUE_TO_GRAV*self.height + (self.zVu**2))
+        print("\nFinal Velocity is " + str(self.zVf))
+        time = abs(self.zVf - self.zVu)
         time = time/self.ACC_DUE_TO_GRAV
         return time
 
     def calcXval(self):
         xVal = self.calcTime()*self.xV
+        self.x = xVal
         return xVal
 
     def finalStringOutput(self):
         xCord = str(self.calcXval())
-        output = "(" + xCord + ", 0)"
+        output = "(" + str(self.x) + ", " + str(self.y) + ", " +str(self.z) + ")"
         return output
 # TODO: Create methods
 
 
 # TODO: Test results
 getX = float (input("\n What's the X velocity: "))
-getZ = float (input("\n What's the magnitude of the Z velocity: "))
+getZ = float (input("\n What's the Z velocity: "))
 getH = float (input("\n What's the height/z-value: "))
 firstDropCalc = dropCalc (getX, getZ, getH)
 print ("\nThe Final result is " + firstDropCalc.finalStringOutput())
